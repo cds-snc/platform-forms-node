5ec2f6e4fa30199c4c9546c8f407a04d06d02c63
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2021-02-25 14:51:19 -0500

    Use i18n Link component in footer

--- components/globals/Footer.js
+++ components/globals/Footer.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { withTranslation } from "../../i18n";
+import { withTranslation, Link } from "../../i18n";
 
 const Footer = ({ t }) => (
   <footer className="gc-footer" data-testid="footer">
@@ -8,10 +8,10 @@ const Footer = ({ t }) => (
       <div>
         <ul>
           <li>
-            <a href={t("footer.privacy.link")}>{t("footer.privacy.desc")}</a>
+            <Link href={"/privacy"}>{t("footer.privacy.desc")}</Link>
           </li>
           <li>
-            <a href={t("footer.terms.link")}>{t("footer.terms.desc")}</a>
+            <Link href={"/terms-avis"}>{t("footer.terms.desc")}</Link>
           </li>
         </ul>
       </div>
